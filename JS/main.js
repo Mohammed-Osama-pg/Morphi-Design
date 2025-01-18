d196111d1756467b6e95f4c5f6f0aa95a0328475
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-01-18 10:26:28 +0300

    add Button function to price table

--- JS/main.js
+++ JS/main.js
@@ -164,3 +164,15 @@ const FitlerPortfolio = (e) => {
 filterButtons.forEach((button) =>
   button.addEventListener("click", FitlerPortfolio)
 );
+
+// !-- The price Table section
+function showPricing(service) {
+  document
+    .querySelectorAll(".pricing-container")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service).classList.add("active");
+  document
+    .querySelectorAll(".nav-buttons button")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service + "-btn").classList.add("active");
+}
