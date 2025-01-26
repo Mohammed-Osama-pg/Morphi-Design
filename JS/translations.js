31d7fd7527f2074d2c64aa55923e826b8393b724
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-01-26 14:35:47 +0300

    Add the header language text
    Add the header language text in data base for Arabic and English

--- JS/translations.js
+++ JS/translations.js
@@ -1,10 +1,32 @@
 // Translation object for all site text
 const translations = {
   // !--------------- Start English ---------------------------
-  en: { headerCurrentLang: "English" },
+  en: {
+    // ?------ Start header ---------------
+    headerNavbarAboutUs: "About Us",
+    headerNavbarServices: "Services",
+    headerNavbarOurwork: "Our Work",
+    headerNavbarPricing: "Pricing",
+    headerCurrentLang: "English",
+    headerLangOptionEnglish: "English",
+    headerLangOptionArabic: "Arabic",
+    headerCtaContactUs: "Contact us",
+    // ?------ End header ---------------
+  },
   // !--------------- End English ---------------------------
   // !--------------- Start Arabic ---------------------------
-  ar: { headerCurrentLang: "العربية" },
+  ar: {
+    // ?------ Start header ---------------
+    headerNavbarAboutUs: "من نحن",
+    headerNavbarServices: "خدماتنا",
+    headerNavbarOurwork: "أعمالنا",
+    headerNavbarPricing: "الاسعار",
+    headerCurrentLang: "العربية",
+    headerLangOptionEnglish: "الأنجليزية",
+    headerLangOptionArabic: "العربية",
+    headerCtaContactUs: "تواصل معنا",
+    // ?------ End header ---------------
+  },
   // !--------------- End Arabic ---------------------------
 };
 
