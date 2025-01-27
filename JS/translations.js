eb5fdaaf5c3dacf3dd1b96c0dfefbc0b7ca4cce0
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-01-27 09:20:01 +0300

    Add About us section Language data

--- JS/translations.js
+++ JS/translations.js
@@ -16,6 +16,14 @@ const translations = {
     heroIntorText: "elevate your architectural presencea with us",
     heroScroallText: "scroll down for more info",
     // ?------ End Hero Section ---------------
+    // ?------ Start About us Section ---------------
+    aboutUsTitle: "About ",
+    aboutUsTitleSpan: "morphi",
+    aboutUsH3: "Morph Your Vision with",
+    aboutUsH3Span: "Morphi Design!",
+    AboutUsPargraph:
+      "We turn your architectural creativity into powerful marketing tools with sleek portfolios, stunning visualizations, and custom websites to elevate your digital presence. Let’s shape your ideas into a brand that matches your ambition!",
+    // ?------ End About us Section ---------------
   },
   // !--------------- End English ---------------------------
   // !--------------- Start Arabic ---------------------------
@@ -34,6 +42,14 @@ const translations = {
     heroIntorText: "أرتقي بحضورك المعماري معنا",
     heroScroallText: "تعرف علينا",
     // ?------ End Hero Section ---------------
+    // ?------ Start About us Section ---------------
+    aboutUsTitle: "ما هي ",
+    aboutUsTitleSpan: "مورفي",
+    aboutUsH3: "غيّر رؤيتك مع",
+    aboutUsH3Span: "تصميم مورفي!",
+    AboutUsPargraph:
+      "نحن نحول إبداعك المعماري إلى أدوات تسويقية قوية من خلال ملفات أعمال أنيقة وتصورات مذهلة ومواقع ويب مخصصة لتعزيز حضورك الرقمي. دعنا نشكل أفكارك في علامة تجارية تتناسب مع طموحاتك!",
+    // ?------ End About us Section ---------------
   },
   // !--------------- End Arabic ---------------------------
 };
