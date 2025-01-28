09ecfc1f19034aea6914b9dc5560bd9d722487af
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-01-28 13:22:31 +0300

    Fix Price Button function

--- JS/main.js
+++ JS/main.js
@@ -167,18 +167,6 @@ filterButtons.forEach((button) =>
   button.addEventListener("click", FitlerPortfolio)
 );
 
-// !-- The price Table section
-function showPricing(service) {
-  document
-    .querySelectorAll(".pricing-container")
-    .forEach((el) => el.classList.remove("active"));
-  document.getElementById(service).classList.add("active");
-  document
-    .querySelectorAll(".nav-buttons button")
-    .forEach((el) => el.classList.remove("active"));
-  document.getElementById(service + "-btn").classList.add("active");
-}
-
 // Languages that require RTL direction
 const rtlLanguages = ["ar"];
 
--- JS/swepper.js
+++ JS/swepper.js
@@ -88,3 +88,15 @@ document.addEventListener("mouseup", dragStop);
 // imgsHolderSwepper.addEventListener("scroll", InfiniteScroll);
 portWrapper.addEventListener("mouseenter", clearTimeout(timeoutId));
 portWrapper.addEventListener("mouseleave", autoPlay);
+
+// !-- The price Table section
+function showPricing(service) {
+  document
+    .querySelectorAll(".pricing-container")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service).classList.add("active");
+  document
+    .querySelectorAll(".nav-buttons button")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service + "-btn").classList.add("active");
+}
