ad09f0f8d7f3cd1534992856567e22f1a5c6b22e
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-01-28 15:57:36 +0300

    Add data language for services
    Add data language for services in html and javascript

--- JS/translations.js
+++ JS/translations.js
@@ -24,6 +24,26 @@ const translations = {
     AboutUsPargraph:
       "We turn your architectural creativity into powerful marketing tools with sleek portfolios, stunning visualizations, and custom websites to elevate your digital presence. Let’s shape your ideas into a brand that matches your ambition!",
     // ?------ End About us Section ---------------
+    // ?------ Start Services section ---------------
+    servicesTitle: "Our",
+    servicesTitleSpan: "Services",
+    servicesPortfolioSpan: "01",
+    servicesPortfolioH3: "Portfolios",
+    servicesPortfolioPargraph:
+      "We design professional architectural portfolios that reflect your identity in an organized and attractive manner, perfect for students and professionals",
+    servicesPresentationSpan: "02",
+    servicesPresentationH3: "Presentation",
+    servicesPresentationPargraph:
+      "We design organized and visually appealing architectural presentations to help you convey your ideas easily to the audience",
+    servicesVisualizationSpan: "03",
+    servicesVisualizationH3: "Visualization",
+    servicesVisualizationPargraph:
+      "We provide realistic 3D renders that highlight the beauty and details of your designs, perfect for both interior and exterior presentations",
+    servicesWebsiteSpan: "04",
+    servicesWebsiteH3: "Website",
+    servicesWebsitePargraph:
+      "We design custom websites that showcase your projects and professional identity in an attractive and responsive way suitable for all devices",
+    // ?------ End Services section ---------------
     // ?------ Start Portfolio section ---------------
     portfolioTitle: "Our",
     portfolioTitleSpan: "Work",
@@ -54,6 +74,26 @@ const translations = {
     AboutUsPargraph:
       "نحن نحول إبداعك المعماري إلى أدوات تسويقية قوية من خلال ملفات أعمال أنيقة وتصورات مذهلة ومواقع ويب مخصصة لتعزيز حضورك الرقمي. دعنا نشكل أفكارك في علامة تجارية تتناسب مع طموحاتك!",
     // ?------ End About us Section ---------------
+    // ?------ Start Services section ---------------
+    servicesTitle: "ما",
+    servicesTitleSpan: "نقدمه لك",
+    servicesPortfolioSpan: "01",
+    servicesPortfolioH3: "معرض الأعمال",
+    servicesPortfolioPargraph:
+      "نقوم بتصميم حافظات معمارية احترافية تعكس هويتك بشكل منظم وجذاب، مثالية للطلاب والمحترفين",
+    servicesPresentationSpan: "02",
+    servicesPresentationH3: "العروض التقديمية",
+    servicesPresentationPargraph:
+      "نقوم بتصميم عروض معمارية منظمة وجذابة بصريًا لمساعدتك على توصيل أفكارك بسهولة إلى الجمهور",
+    servicesVisualizationSpan: "03",
+    servicesVisualizationH3: "التصوير والتقديم",
+    servicesVisualizationPargraph:
+      "نحن نقدم عروضًا ثلاثية الأبعاد واقعية تبرز جمال وتفاصيل تصميماتك، وهي مثالية للعروض الداخلية والخارجية",
+    servicesWebsiteSpan: "04",
+    servicesWebsiteH3: "موقع إلكتروني",
+    servicesWebsitePargraph:
+      "نقوم بتصميم مواقع ويب مخصصة تعرض مشاريعك وهويتك المهنية بطريقة جذابة ومتجاوبة ومناسبة لجميع الأجهزة",
+    // ?------ End Services section ---------------
     // ?------ Start Portfolio section ---------------
     portfolioTitle: "معرض",
     portfolioTitleSpan: "أعمالنا",
