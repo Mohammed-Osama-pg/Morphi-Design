e0321f73f80bd78e86942514b721f5b5e8acd952
Mohammed-Osama-pg <141535887+Mohammed-Osama-pg@users.noreply.github.com>
2025-02-04 16:37:29 +0300

    Reorder JavaScript file

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,15 @@
 import translations from "../JS/translations.js";
 ("use strict");
 
-/**
- * Add event on multiple elements
- */
+// !-- Navbar Toggle for mobile ----------------------------------------
+
+// *Add event on multiple elements
 const addEventOnElement = function (elements, eventType, callback) {
   elements.forEach((element) => {
     element.addEventListener(eventType, callback);
   });
 };
-
-/**
- * Navbar Toggle for mobile
- */
+// *Calls all elements
 const navbar = document.querySelector("[data-navbar]");
 const navbarToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -27,15 +24,16 @@ const toggleNavbar = function () {
     document.body.classList.toggle("nav-active");
   }
 };
+// *Calls the function
 addEventOnElement(
   [navbarToggleBtn, overlay, navbarList, contactBtn],
   "click",
   toggleNavbar
 );
 
-/**
- * Services carousel movement
- */
+// !-- Services carousel movement ----------------------------------------
+
+// *Calls all elements
 const services = Array.from(document.querySelectorAll("[data-service]"));
 const carousel = document.querySelector(".carousel");
 const nextBtn = document.querySelector("#next-serv");
@@ -44,7 +42,7 @@ const length = services.length;
 const imgSilder = document.querySelectorAll(".imge-container img");
 let conte = 1;
 
-// Create pagination (ul and li elements)
+// *Create pagination (ul and li elements)
 const ul = document.createElement("ul");
 ul.setAttribute("id", "ul-id");
 nextBtn.before(ul);
@@ -66,7 +64,7 @@ liElements.forEach((li) => {
   });
 });
 
-// Set up auto-rotation with an interval
+// *Set up auto-rotation with an interval
 let intervalId = setInterval(() => {
   if (conte >= length) {
     conte = 1;
@@ -76,10 +74,12 @@ let intervalId = setInterval(() => {
   check();
 }, 5000);
 
-//  Stop auto-rotation
+//  *Stop auto-rotation
 function stopAutoRotation() {
   clearInterval(intervalId);
 }
+
+// *All the functions
 function check() {
   // Update service slides
   services.forEach((service, index) => {
@@ -130,7 +130,7 @@ function prev() {
   }
 }
 
-// Attach button event listeners
+// *Attach button event listeners
 nextBtn.addEventListener("click", () => {
   next();
   stopAutoRotation();
@@ -140,14 +140,16 @@ previousBtn.addEventListener("click", () => {
   stopAutoRotation();
 });
 
-// Initialize the carousel
+// *Initialize the carousel
 check();
 
-// Select All filter buttons and filterable card
+// !-- Portfolio fillter ----------------------------------------
+
+// *Select All fillter buttons and fillterable card
 const filterButtons = document.querySelectorAll(".btn-holder button");
 const portfolioImages = document.querySelectorAll(".imgs-holder .port-card");
 
-// Define the filterPortfolio function
+// *Define the filterPortfolio function
 const FitlerPortfolio = (e) => {
   document
     .querySelector(".btn-holder .btn-third.active")
@@ -167,16 +169,87 @@ const FitlerPortfolio = (e) => {
   });
 };
 
-// Add click Event Listener to each filter button
+// *Add click Event Listener to each filter button
 filterButtons.forEach((button) =>
   button.addEventListener("click", FitlerPortfolio)
 );
 
-// Languages that require RTL direction
+// !Swepper functions ---------------------------------
+
+const portWrapper = document.querySelector(".wrapper");
+const imgsHolderSwepper = document.querySelector(".imgs-holder");
+const portArrowBtnSwepper = document.querySelectorAll(".wrapper ion-icon");
+let firstCardWidth = imgsHolderSwepper.querySelector(".port-card").offsetWidth;
+let isDragging = false,
+  startX,
+  startScrollleft,
+  timeoutId;
+portArrowBtnSwepper.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    imgsHolderSwepper.scrollLeft +=
+      btn.id === "left-arrow" ? -firstCardWidth : firstCardWidth;
+  });
+});
+const dragStart = (e) => {
+  isDragging = true;
+  imgsHolderSwepper.classList.add("dragging");
+  // Records the intial cursor and scroll positono of the img_holder
+  startX = e.pageX;
+  startScrollleft = imgsHolderSwepper.scrollLeft;
+};
+const dragStop = () => {
+  isDragging = false;
+  imgsHolderSwepper.classList.remove("dragging");
+};
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return form here
+  // update the scrooll position of the carouel based on the cursor movment
+  imgsHolderSwepper.scrollLeft = startScrollleft - (e.pageX - startX);
+};
+imgsHolderSwepper.addEventListener("mousedown", dragStart);
+imgsHolderSwepper.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+// !-- The price Table section ----------------------------------
+function showPricing(service) {
+  document
+    .querySelectorAll(".pricing-container")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service).classList.add("active");
+  document
+    .querySelectorAll(".nav-buttons button")
+    .forEach((el) => el.classList.remove("active"));
+  document.getElementById(service + "-btn").classList.add("active");
+}
+
+// !-- Preload functions ----------------------------------
+// call all animation element
+let preLoade = document.querySelector(".pre-loade");
+let allAnimation = document.querySelectorAll(".animation");
+// *Create pre-load fuction
+window.addEventListener("load", () => {
+  preLoade.classList.add("fade-out");
+  allAnimation.forEach(function (ele) {
+    if (window.innerWidth < 768) {
+      const navbar = document.querySelector(".header .navbar");
+      const CTA = document.querySelector(".header .cta");
+      navbar.classList.remove("animation");
+      CTA.classList.remove("animation");
+      allAnimation = document.querySelectorAll(".animation");
+      ele.style.animationPlayState = "running";
+    } else {
+      ele.style.animationPlayState = "running";
+    }
+  });
+});
+
+// !-- Language selector ----------------------------------------
+
+// *Languages that require RTL direction
 const rtlLanguages = ["ar"];
 const portArrowBtn = document.querySelectorAll(".wrapper ion-icon");
 
-// Function to set the selected language and update the site
+// *Function to set the selected language and update the site
 function setLanguage(languageCode) {
   // Update displayed current language
   const currentLangBtn = document.getElementById("current-lang");
@@ -195,7 +268,7 @@ function setLanguage(languageCode) {
   updateSiteDirection(languageCode);
 }
 
-// Function to update text content dynamically
+// *Function to update text content dynamically
 function updateSiteText(languageCode) {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
     const i18n = element.getAttribute("data-i18n");
@@ -212,7 +285,7 @@ function updateSiteText(languageCode) {
     });
 }
 
-// Function to update site direction (LTR/RTL)
+// *Function to update site direction (LTR/RTL)
 function updateSiteDirection(languageCode) {
   const isRTL = rtlLanguages.includes(languageCode);
   const socialMediaText = document.querySelector(".contact .info .social");
@@ -238,7 +311,7 @@ function updateSiteDirection(languageCode) {
   check();
 }
 
-// Event listeners for language options
+// *Event listeners for language options
 document.querySelectorAll(".lang-option").forEach((option) => {
   option.addEventListener("click", (event) => {
     const selectedLang = event.target.getAttribute("data-lang");
@@ -246,82 +319,8 @@ document.querySelectorAll(".lang-option").forEach((option) => {
   });
 });
 
-// Set the default or saved language on page load
+// *Set the default or saved language on page load
 document.addEventListener("DOMContentLoaded", () => {
   const savedLanguage = localStorage.getItem("selectedLanguage") || "en"; // Default to English
   setLanguage(savedLanguage);
-});
-
-// Swepper functions ---------------------------------
-
-const portWrapper = document.querySelector(".wrapper");
-const imgsHolderSwepper = document.querySelector(".imgs-holder");
-const portArrowBtnSwepper = document.querySelectorAll(".wrapper ion-icon");
-let firstCardWidth = imgsHolderSwepper.querySelector(".port-card").offsetWidth;
-let isDragging = false,
-  startX,
-  startScrollleft,
-  timeoutId;
-
-portArrowBtnSwepper.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    imgsHolderSwepper.scrollLeft +=
-      btn.id === "left-arrow" ? -firstCardWidth : firstCardWidth;
-  });
-});
-
-const dragStart = (e) => {
-  isDragging = true;
-  imgsHolderSwepper.classList.add("dragging");
-  // Records the intial cursor and scroll positono of the img_holder
-  startX = e.pageX;
-  startScrollleft = imgsHolderSwepper.scrollLeft;
-};
-
-const dragStop = () => {
-  isDragging = false;
-  imgsHolderSwepper.classList.remove("dragging");
-};
-
-const dragging = (e) => {
-  if (!isDragging) return; // if isDragging is false return form here
-  // update the scrooll position of the carouel based on the cursor movment
-  imgsHolderSwepper.scrollLeft = startScrollleft - (e.pageX - startX);
-};
-
-imgsHolderSwepper.addEventListener("mousedown", dragStart);
-imgsHolderSwepper.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-
-// !-- The price Table section
-function showPricing(service) {
-  document
-    .querySelectorAll(".pricing-container")
-    .forEach((el) => el.classList.remove("active"));
-  document.getElementById(service).classList.add("active");
-  document
-    .querySelectorAll(".nav-buttons button")
-    .forEach((el) => el.classList.remove("active"));
-  document.getElementById(service + "-btn").classList.add("active");
-}
-
-// !-- Preload functions ----------------------------------
-// call all animation element
-let preLoade = document.querySelector(".pre-loade");
-let allAnimation = document.querySelectorAll(".animation");
-// Create pre-load fuction
-window.addEventListener("load", () => {
-  preLoade.classList.add("fade-out");
-  allAnimation.forEach(function (ele) {
-    if (window.innerWidth < 768) {
-      const navbar = document.querySelector(".header .navbar");
-      const CTA = document.querySelector(".header .cta");
-      navbar.classList.remove("animation");
-      CTA.classList.remove("animation");
-      allAnimation = document.querySelectorAll(".animation");
-      ele.style.animationPlayState = "running";
-    } else {
-      ele.style.animationPlayState = "running";
-    }
-  });
 });
